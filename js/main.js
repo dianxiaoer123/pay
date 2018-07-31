95c6623b06341d29d7d9a0cdae02aed832bbbf47
yuxia <3451549980@qq.com>
2018-07-31 19:39:53 +0800

    'new'

--- js/main.js
+++ js/main.js
@@ -1,25 +1,46 @@
 $( document ).ready(function() {
-	$('.base-slider, .owl-stage').owlCarousel({
-		loop: true,
+	$('.base-slider .owl-stage').owlCarousel({
+		loop:true,
 		margin: 0,
 		nav: true,
 		autoPlay: true,
 		autoPlay: 3000,
 		navText: ["<img src='images/arrow-left.png'>","<img src='images/arrow-right.png'>"],
 		dots: false,
-		items: 1,
+		items:1,
 		responsive:{
 			0:{
-				items:1
-			},
+			 items:1
+		  },
 			600:{
-				items:1
-			},
-			1000:{
-				items:1
-			}
-		}
+			 items:1
+		  },
+		  1000:{
+			 items:1
+		  }
+	}
 	});
+	// $('.base-slider, .owl-stage').owlCarousel({
+	// 	loop: true,
+	// 	margin: 0,
+	// 	nav: true,
+	// 	autoPlay: true,
+	// 	autoPlay: 3000,
+	// 	navText: ["<img src='images/arrow-left.png'>","<img src='images/arrow-right.png'>"],
+	// 	dots: false,
+	// 	items: 1,
+	// 	// responsive:{
+	// 	// 	0:{
+	// 	// 		items:1
+	// 	// 	},
+	// 	// 	600:{
+	// 	// 		items:1
+	// 	// 	},
+	// 	// 	1000:{
+	// 	// 		items:1
+	// 	// 	}
+	// 	// }
+	// });
 
 	$('.mobile-menu-btn').on('click', function(){
 		$(this).toggleClass('active');
